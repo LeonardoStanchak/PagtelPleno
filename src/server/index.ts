e116c91fb43570b0d34339e095d3c8fdbd77cb0d
Leonardo Stanchak <leonardostanchak@hotmail.com>
2022-08-24 17:20:11 -0300

    Criacao do rabbit mq e do docker compose

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import rabbitMqRoutes from "./../routes/rabbitmq-routes"
 
 import router from '../routes/routes'
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
+
+app.use('/', rabbitMqRoutes)
 
 app.listen(3333, () => {
     console.log('Server running on port 3333');
